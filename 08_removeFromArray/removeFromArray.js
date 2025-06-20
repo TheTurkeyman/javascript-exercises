917d1dfde4a6f86052031578cadae561a7fc1ddd
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-06-20 22:56:41 +0300

    Update removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,20 @@
-const removeFromArray = function(a,b) {
-let f = a.filter(item => item !== b);
-return f;
 
+
+const removeFromArray = function(a,b) {
+return a.filter(f => !b.includes(f));
 };
 
+/*let newarr = []
+
+a.forEach(Tuki) {
+if (!b.icludes(Tuki)){
+    newarr.push(Tuki);}
+    
+    return newArr;
+    }; */
+
+  //removeFromArray([1,2,3,4,5,6] 3,5,1);
 // Do not edit below this line
+
 module.exports = removeFromArray;
+
