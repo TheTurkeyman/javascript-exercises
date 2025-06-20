8473e8dc15680e9bad2661406587357aa1ff7528
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-06-20 23:20:15 +0300

    Update sumAll.js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+  let result = 0;
+  if (a > b) {
+for (let i = a; i >= b; i--)
+  result = a + b +i;
+}
+else {
+for (let i = b; i >= a; i--)
+  result = a + b + i;
 };
+  return result; }
 
 // Do not edit below this line
 module.exports = sumAll;
