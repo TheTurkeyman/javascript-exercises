a0174096e7898928cef8c71c789614d58922dcb8
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-06-20 13:27:27 +0300

    Update reverseString.js

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  let a = "";
+for (i=str.length-1; i>=0; i--)
+  a+=str[i]
+    return a;
 };
 
 // Do not edit below this line
