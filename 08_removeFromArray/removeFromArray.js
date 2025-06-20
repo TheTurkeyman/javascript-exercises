b49e594d689d5c50d010fe7277559bedf62b878c
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-06-20 22:30:40 +0300

    Update removeFromArray.js

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
 const removeFromArray = function(a,b) {
-let f = a.find(item => item === item(b);
-let g = a.slice(f, 1)
-return g;
+let f = a.filter(item => item !== b);
+return f;
+
 };
 
 // Do not edit below this line
