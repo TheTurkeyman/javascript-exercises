3bf09e685b801ac58e4cdd781f3fbd0238e9f03e
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-06-21 18:18:54 +0300

    Update tempConversion.js

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+ let C = (f - 32) / 1.8;
+ return C.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let F = c * 1.8 + 32;
+  return F.toFixed(1);
 };
 
 // Do not edit below this line
