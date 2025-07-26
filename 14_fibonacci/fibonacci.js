394d1129d1fafe3952e18db2071bae1e6303f802
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-07-26 15:23:50 +0300

    Update fibonacci.js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  const numtonum = Number(num);
+ if (numtonum < 0) {
+        return;
+    }
+    if (numtonum === 0) {
+        return 0;
+    }
+ let sequence = [0, 1];
+for (let i = 2; i <= numtonum; i++){
+  let addtosequence = sequence[i - 1] + sequence[i - 2];
+  sequence.push(addtosequence);
+}
+  let seqnum = sequence.at(numtonum);
+  return seqnum;
 
 };
 
