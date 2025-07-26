719657987956c5c8755bb2170e9db606828c5cec
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-07-26 14:40:09 +0300

    Update palindromes.js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,8 @@
-const palindromes = function () {
+const palindromes = function (a) {
+ let casefix = a.toLowerCase().replace(/[^a-z0-9]/g, "");
+ let reversed = casefix.split('').reverse().join(''); 
+return reversed === casefix;
+  
 
 };
 
