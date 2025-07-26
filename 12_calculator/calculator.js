a35fcdc83d38265471a2926b9c94e53590101c14
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-07-26 14:10:59 +0300

    Update calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a,b) {
+	return a + b;
+	
 	
 };
 
-const subtract = function() {
+const subtract = function(a,b) {
+	return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+	return arr.reduce((a,b) => a + b, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(a,b) {
+	return a * b;
 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+	if (num === 0 || num === 1)
+		return 1;
+	for (let i = num - 1; i >= 1; i--)
+	{ num *= i};
+	return num;
 	
 };
 
