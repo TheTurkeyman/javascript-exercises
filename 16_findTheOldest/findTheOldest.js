9dd1a486645aa1c73bfcd997d97f0ad25f5c2a9f
TheTurkeyman <160347759+TheTurkeyman@users.noreply.github.com>
2025-07-27 21:12:50 +0300

    Update findTheOldest.js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) 
+{
+const getAge = function (dateofbirth, dateofdeath) {
+  if (!dateofdeath) {
+    dateofdeath = new Date().getFullYear();
+  }
+  return dateofdeath - dateofbirth;
+};
 
+let calculatedage = people.map((item) => getAge(item.yearOfBirth, item.yearOfDeath));
+ people.forEach((item, index) => {item.age = calculatedage[index]});
+ people.sort((a, b) => b.age - a.age);
+return people.at(0);
+}
 };
 
 // Do not edit below this line
